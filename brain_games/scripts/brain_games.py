488e3af1c6e76f5418ac7ac4e9f8bf1b78527fb4
AnyaMankova <ao.mankova@yandex.ru>
2022-06-06 23:12:01 +1100

    files update to PEP8

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,12 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
+"""Main function if the game."""
 from brain_games.cli import welcome_user
 
 
 def main():
+    """Greet user."""
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
